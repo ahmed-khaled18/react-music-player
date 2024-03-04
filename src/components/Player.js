9cee390bd861405fc52547df2e0bed341cf4f393
Ahmed Khaled <ahmed@mrkhalednasr.com>
2024-03-04 13:41:02 +0200

    time update

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,9 +1,13 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlay, faAngleLeft, faAngleRight } from "@fortawesome/free-solid-svg-icons";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 const Player = ({ currentSong, isPlaying, setIsPlaying }) => {
   const audioRef = useRef(null);
+  const [songInfo, setSongInfo] = useState({
+    currentTime: null,
+    duration: null,
+  });
   const playSongHandler = () => {
     if (isPlaying) {
       audioRef.current.pause();
@@ -13,19 +17,28 @@ const Player = ({ currentSong, isPlaying, setIsPlaying }) => {
       setIsPlaying(!isPlaying);
     }
   };
+  const timeHandler = (e) => {
+    const currentTime = e.target.currentTime;
+    const duration = e.target.duration;
+    setSongInfo({ ...songInfo, currentTime: currentTime, duration: duration });
+  };
+
+  const timeFormatter = (time) => {
+    return Math.floor(time / 60) + ":" + ("0" + Math.floor(time % 60)).slice(-2);
+  };
   return (
     <div className="player-controller-container">
       <div className="time-controller-container">
-        <p>start time</p>
+        <p>{timeFormatter(songInfo.currentTime)}</p>
         <input type="range" />
-        <p>end time</p>
+        <p>{timeFormatter(songInfo.duration)}</p>
       </div>
       <div className="control-buttons-container">
         <FontAwesomeIcon icon={faAngleLeft} size="2x" />
         <FontAwesomeIcon onClick={playSongHandler} icon={faPlay} size="2x" />
         <FontAwesomeIcon icon={faAngleRight} size="2x" />
       </div>
-      <audio ref={audioRef} src={currentSong.audio}></audio>
+      <audio onLoadedMetadata={timeHandler} onTimeUpdate={timeHandler} ref={audioRef} src={currentSong.audio}></audio>
     </div>
   );
 };
