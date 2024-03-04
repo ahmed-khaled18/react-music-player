e1c51b2c4b7bbd6ac76539a6dce81a7fe47270ae
Ahmed Khaled <ahmed@mrkhalednasr.com>
2024-03-04 12:51:56 +0200

    adding states

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import "./styles/app.scss";
 import Player from "./components/Player";
 import Song from "./components/Song";
 import data from "./data";
+import { useState } from "react";
 function App() {
+  const [songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(songs[3]);
   return (
     <div className="app">
-      <Song />
+      <Song currentSong={currentSong} />
       <Player />;
     </div>
   );
