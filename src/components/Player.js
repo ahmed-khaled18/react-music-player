1374ca3603daf12f1a4c6cbc756f0fcefb13ccc0
Ahmed Khaled <ahmed@mrkhalednasr.com>
2024-03-04 14:12:31 +0200

    add slider functionallity

--- src/components/Player.js
+++ src/components/Player.js
@@ -26,11 +26,16 @@ const Player = ({ currentSong, isPlaying, setIsPlaying }) => {
   const timeFormatter = (time) => {
     return Math.floor(time / 60) + ":" + ("0" + Math.floor(time % 60)).slice(-2);
   };
+
+  const dragHandller = (e) => {
+    audioRef.current.currentTime = e.target.value;
+    setSongInfo({ ...songInfo, currentTime: e.target.value });
+  };
   return (
     <div className="player-controller-container">
       <div className="time-controller-container">
         <p>{timeFormatter(songInfo.currentTime)}</p>
-        <input type="range" />
+        <input onChange={dragHandller} min={0} max={songInfo.duration} value={songInfo.currentTime} type="range" />
         <p>{timeFormatter(songInfo.duration)}</p>
       </div>
       <div className="control-buttons-container">
